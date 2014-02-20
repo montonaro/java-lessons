e580117fc1ea911f8d954cf184fa81f6dedfb65e
montonaro <montonaro@gmail.com>
2014-02-21 00:46:58 +0200

    Update TicTacToe.java

--- task4/TicTacToe.java
+++ task4/TicTacToe.java
@@ -1,17 +1,20 @@
 package task4;
-
-import java.util.Arrays;
+ 
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class TicTacToe {
 	
-	private int x = 3;
-	private int y = 3;	
+	private final int longStep = 3; // длина хода
+	private int x = 3; //размер поля x
+	private int y = 3; //размер поля y	
+	
+	private int userX = 0;  // ход пользователя x
+	private int userY = 0;  // ход пользователя y
 	
 	private char[][] field;
 	
-	private int activeUser = 0;
+	private int activeUser = 0; // активный пользователь
 
 	public TicTacToe(int x, int y){ 
 		
@@ -24,6 +27,11 @@ public class TicTacToe {
 		
 		field 		= new char[this.y][this.x];		
 		activeUser 	= 1;
+	}
+	
+	public void setUserCoors(int x, int y){
+		userX = x;
+		userY = y;
 	}
 	
 	public void show(){
@@ -49,31 +57,180 @@ public class TicTacToe {
 		}
 	}
 	
-	public boolean checkEmpty(int x, int y){
+	/**
+	 * @return символ активного пользователя
+	 */
+	private char getUserSymbol(){
+		if(activeUser == 1){
+			return '0'; 
+		} else if(activeUser == 2){			
+			return 'X'; 
+		}
 		
-		boolean status = false; 
+		return ' ';
+	}
+	
+	/*	
+	 * Проверка рекурсией
+	 */
+	private boolean countSteps(int x, int y, String way, int count){ 
+		//System.out.println(field[y-1][x] + " "+ getUserSymbol()+ " x-"+x + " Y-"+(y-1));
 		
-		if(x < (this.x-1) && y < (this.y-1)){
+		switch(way){
+			case "up": 
+				if(checkEmpty(x,y-1) >= 0){  
+					//если следующий такой же символ
+					if(field[y-1][x] == getUserSymbol()){  						
+						if(count+1 == longStep-1){ //достигнуто максимальное количество
+							return true;
+						} else {
+							return countSteps(x, y-1, way, count+1);
+						} 
+					}
+				}   
+				break;
+			case "up-left": 
+				if(checkEmpty(x-1,y-1) >= 0){  
+					//если следующий такой же символ
+					if(field[y-1][x-1] == getUserSymbol()){  						
+						if(count+1 == longStep-1){ //достигнуто максимальное количество
+							return true;
+						} else {
+							return countSteps(x-1, y-1, way, count+1);
+						} 
+					}
+				}   
+				break;
+			
+			case "up-right": 
+				if(checkEmpty(x+1,y-1) >= 0){  
+					//если следующий такой же символ
+					if(field[y-1][x+1] == getUserSymbol()){  						
+						if(count+1 == longStep-1){ //достигнуто максимальное количество
+							return true;
+						} else {
+							return countSteps(x+1, y-1, way, count+1);
+						} 
+					}
+				}   
+				break;
+				
+			case "down": 
+				if(checkEmpty(x,y+1) >= 0){  
+					//если следующий такой же символ
+					if(field[y+1][x] == getUserSymbol()){  						
+						if(count+1 == longStep-1){ //достигнуто максимальное количество
+							return true;
+						} else {
+							return countSteps(x, y+1, way, count+1);
+						} 
+					}
+				}   
+				break;
+				
+			case "down-left":   
+				if(checkEmpty(x-1,y+1) >= 0){  
+					//если следующий такой же символ
+					if(field[y+1][x-1] == getUserSymbol()){  						
+						if(count+1 == longStep-1){ //достигнуто максимальное количество
+							return true;
+						} else {
+							return countSteps(x-1, y+1, way, count+1);
+						} 
+					}
+				}   
+				break;
+				 
+			case "down-right":  
+				if(checkEmpty(x+1,y+1) >= 0){  
+					//если следующий такой же символ
+					if(field[y+1][x+1] == getUserSymbol()){  						
+						if(count+1 == longStep-1){ //достигнуто максимальное количество
+							return true;
+						} else {
+							return countSteps(x+1, y+1, way, count+1);
+						} 
+					}
+				}   
+				break;
+				
+			case "left": 
+				if(checkEmpty(x-1,y) >= 0){  
+					//если следующий такой же символ
+					if(field[y][x-1] == getUserSymbol()){  						
+						if(count+1 == longStep-1){ //достигнуто максимальное количество
+							return true;
+						} else {
+							return countSteps(x-1, y, way, count+1);
+						} 
+					}
+				}   
+				break;
+				
+			case "right": 
+				if(checkEmpty(x+1,y) >= 0){  
+					//если следующий такой же символ
+					if(field[y][x+1] == getUserSymbol()){  						
+						if(count+1 == longStep-1){ //достигнуто максимальное количество
+							return true;
+						} else {
+							return countSteps(x+1, y, way, count+1);
+						} 
+					}
+				}   
+				break;
+		}
+		return false;
+	}
+	
+	
+	/** 
+	 *  Проверка на конец игры
+	 *  ранее проверено что ячейка пуста и не выходит за предел поля
+	 */
+	private boolean checkStop(){
+		
+		String [] arr = new String[]{"up", "down", "left", "right", "up-left", "up-right", "down-left", "down-right"};
+		
+		for(int i = 0; i < arr.length; i++){
+			if(countSteps(userX, userY, arr[i], 0)){
+				return true;
+			}
+		} 
+		
+		return false;
+	}
+	
+	/**	 
+	 * @return int 0 - занято, -1 - выход за диапазон, 1 - пусто 
+	 */
+	public int checkEmpty(int x, int y){
+		
+		int status = 0;  
+		
+		if((x >= 0 && y >= 0) && (x < this.x && y < this.y)){
 			
 			if(field[y][x] == 0){
-				status = true;
-			}
+				status = 1;
+			} 
 			
-		}	 
+		} else {	 
+			status = -1; // выход за диапазон
+		}
 		
 		return status;
 	}
 	
 	/**
-	 * Делаем ход.
+	 * Делаем ход. 
 	 */
-	public void setStep(int x, int y){ 
-		
+	private void setStep(){ 
+	
 		if(activeUser == 1){
-			field[y][x] = '0';
+			field[userY][userX] = getUserSymbol();
 			activeUser = 2;
 		} else if(activeUser == 2){			
-			field[y][x] = 'X';
+			field[userY][userX] = getUserSymbol();
 			activeUser = 1;
 		}
 	}
@@ -133,11 +290,23 @@ public class TicTacToe {
 							int userX = Integer.parseInt(coors[0]);
 							int userY = Integer.parseInt(coors[1]); 
 							
-							if(obj.checkEmpty(userX, userY)){
+							obj.setUserCoors(userX, userY);
+							
+							int status = obj.checkEmpty(userX, userY);
+							
+							//switch
+							if(status > 0){				
 								
-								obj.setStep(userX, userY);
+								if(obj.checkStop()){
+									System.out.println("Кoнец. Победа игрока №"+obj.activeUser); 
+									break;
+								} 
+								
+								obj.setStep();  
 								obj.show();
 								
+							} else if(status == -1){
+								System.out.println("Выход за диапазон"); 
 							} else {
 								System.out.println("Желаемое вами поле - занято"); 
 							}		 
