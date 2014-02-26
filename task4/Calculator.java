225ecca7131da4c3836e77ee5c12b344f2bc3bf2
montonaro <montonaro@gmail.com>
2014-02-26 18:18:40 +0200

    Update Calculator.java

--- task4/Calculator.java
+++ task4/Calculator.java
@@ -1,47 +1,109 @@
 package task4;
  
+
 import java.util.Scanner;
 import java.util.Stack;
 
 public class Calculator {
-	
+
 	private Stack<String> stack = null;
 	private String output = "";
-	
+
 	public static boolean isOperator(char str){ 
-		
+
 		switch(str){
 			case '+':
 			case '-':
 			case '^':
 			case '/':
 			case '*':	
-			case '(':	
-			case ')':	
 				return true; 
 			default:
 				return false; 
 		} 	
 	}  
- 
-	public void parseString(String str) throws Exception{
-		 		
+	
+	public void calculation(String str) throws Exception{
+		
+		Stack<String> stack = new Stack<String>();  
+		
 		if(str.length() == 0 || str.replaceAll(" ", "").length() == 0 ){
-			throw new Exception("Пустая строка");
+			throw new Exception("Пришла пустая строка для вычисления");
+		}
+		
+		char tmp = 0; 
+		
+		String [] strArr = str.split(" ");
+		
+		for(int i = 0; i < str.length(); i++){
+			
+			if(strArr[i].length() == 1){ // может быть символ
+				tmp = strArr[i].charAt(i);
+				
+				try {
+					int var = Integer.parseInt(stack.pop());
+					
+					stack.push(""+var);  
+		        } catch (NumberFormatException e) {
+ 
+		        	if(isOperator(tmp)){
+						
+						if (stack.size() < 2)
+							throw new Exception("Недостаточно данных в стеке для операции "+tmp);
+						
+						int a 		= Integer.parseInt(stack.pop()); // тут полюбому число
+						int b 		= Integer.parseInt(stack.pop()); // тут полюбому число				
+						int res  	= 0;
+						
+						switch (tmp){
+							case '*': res = a*b; break;
+							case '/': res = a/b; break;
+							case '+': res = a+b; break;
+							case '-': res = a-b; break;
+						}
+						
+						stack.push(""+res);  
+					}
+		        }   
+				
+			} 
+
+			try {
+				int var = Integer.parseInt(stack.pop());  
+				stack.push(""+var);  
+			} catch (NumberFormatException e) {  
+				throw new Exception("Недопустимый символ в выражении: "+tmp);
+			}     
+		}
+		
+		if (stack.size() > 1)
+			throw new Exception("Количество операторов не соответствует количеству операндов");
+		
+		System.out.print(" : " + stack.pop());	
+	}
+	
+	/**	
+	 * @param str - заранее отформатированная строка, вида: "3 4 5 * + 7 + 4 6 - /"
+	 * @throws Exception
+	 */
+	private void parseString(String str) throws Exception{
+
+		if(str.length() == 0 || str.replaceAll(" ", "").length() == 0 ){
+			throw new Exception("Пришла пустая строка для парсинга");
 		}
  
 		char tmp = 0; 
-		
+
 		for(int i = 0; i < str.length(); i++){  
-			
+
 			tmp = str.charAt(i);
 			if(tmp == ' '){
 				continue;
 			} else if(Character.isDigit(tmp)){
 				output += tmp;
-			} else if(isOperator(tmp)){
+			} else if(isOperator(tmp) || tmp == '(' || tmp == ')'){
 				output += " ";
-				
+
 				switch(tmp){
 		            case '+':               
 		            case '-':
@@ -68,7 +130,7 @@ public class Calculator {
 	}
    
 	public  void gotParen(char ch){  
-		 
+
 		while(!stack.isEmpty()){ 
 			String chx = stack.pop();
 			if( chx.equals('('))            
@@ -77,15 +139,15 @@ public class Calculator {
 				output = output +" "+ chx;  // output it
 		}  // end while		 
 	}  
-	
+
 	public String getString(){
 		return  output + " "+  stack.pop();
 	}
-	
+
 	public void consider(char ch, int status){    
-	 
+
 		while(!stack.isEmpty()){ 
-			 
+
 			String opTop = stack.pop();
        
 			if( opTop.equals('(')){	
@@ -98,7 +160,7 @@ public class Calculator {
 	        		status2 = 1;
 		        else
 		        	status2 = 2;
-	        	
+
 	        	if(status2 < status) {                     
 	        		stack.push(opTop);  
 	        		break;
@@ -107,62 +169,40 @@ public class Calculator {
 	        	}
 	        }  
 		}  // end while	 
-		
+
 		stack.push(""+ch);           
-    	}  
-	 
-	
+    }  
+
+
 	public Calculator(String str){
-		
+
 		stack = new Stack<String>();  
-		
+
 		try {
 			parseString(str);
 			System.out.println("" + getString() );
+			
+			calculation(str);
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}	
-	
-	
-	//@SuppressWarnings("resource")
+
+ 
 	public static void main(String [] args) { 
- 		
- 		System.out.print("Введите ваше уравнение :");	
- 		
+		
+		System.out.print("Введите ваше уравнение :");	 
+		
 		Scanner sc = new Scanner(System.in);  
 		while(true){ 	 
-		  
+
 			String sentence = sc.nextLine(); 
 			Calculator obj = new Calculator(sentence);	
 			//System.out.println(sentence);	 
-			 
-			
-	} // end while  
-	
-	/**	
-	 * @param str - заранее отформатированная строка, вида: "3 4 5 * + 7 + 4 6 - /"
-	 * @throws Exception
-	 */
-	public void calculation(String str) throws Exception{
-		
-		if(str.length() == 0 || str.replaceAll(" ", "").length() == 0 ){
-			throw new Exception("Пришла пустая строка для вычисления");
-		}
-		char tmp = 0; 
-		
-		for(int i = 0; i < str.length(); i++){
-			
-			tmp = str.charAt(i);
-			if(isOperator(tmp)){
-				
-			} else if(Character.isDigit(tmp)){
-				
-				
-			} else {
-				throw new Exception("Недопустимый символ в выражении: "+tmp);
-			}  
-		}
-	}
+
+
+		} // end while  
+
+   }
 }
