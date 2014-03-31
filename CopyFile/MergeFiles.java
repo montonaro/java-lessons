525ea7991de68119fa791b41c7e100694649f957
montonaro <montonaro@gmail.com>
2014-03-31 14:31:08 +0300

    Update MergeFiles.java

--- CopyFile/MergeFiles.java
+++ CopyFile/MergeFiles.java
@@ -1,22 +1,40 @@
-﻿package CopyFile;
+package copyFile;
 
 import java.io.*;
 
 public class MergeFiles extends MyFiles { 
 	
-	public void merge(String src1, String src2, String dest) {
-		  
+	public void merge(String src1, String src2, String dest) throws IOException {
+		
+		SequenceInputStream  in   	= null;
+		BufferedOutputStream out  	= null;
+		FileInputStream  f1   		= null;
+		FileInputStream f2  		= null;
+		
 		try{
 			checkFile(src1); 
 			checkFile(src2);  
-			in = new SequenceInputStream(new FileInputStream(src1), new FileInputStream(src2));
+			checkDest(dest); 
 			
-			checkDest(dest);  
+			f1 = new FileInputStream(src1);
+			f2 = new FileInputStream(src2);
+			
+			in = new SequenceInputStream(f1, f2);		
 			out = new BufferedOutputStream(new FileOutputStream(dest));
 			
-			copy();
+			int marker = 0;
+			while( (marker = in.read()) != -1){  
+	            out.write(marker);  
+	        } 
+			
 		} catch (Exception e){
    	       System.out.println(e.getMessage()); 
-   	    } 
+		}finally { 
+		 
+			f1.close(); 
+			f2.close(); 
+			in.close();	 
+			out.close();			 
+	    } 
 	} 
 }
