67b62490a58fa40656025e8b973b5f5507125230
montonaro <montonaro@gmail.com>
2014-03-31 14:30:56 +0300

    31.03.2014

--- CopyFile/Main.java
+++ CopyFile/Main.java
@@ -1,13 +1,17 @@
-﻿package CopyFile; 
+package copyFile; 
+
+import java.io.IOException;
  
 
 public class Main {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		
 		CopyFile c = new CopyFile(); 
 		c.copyFile("c:\\tmp\\1.txt", "c:\\tmp\\dir\\1.txt");
-
+		System.out.println("Операция копирования завершена."); 
+		
 		MergeFiles m = new MergeFiles(); 
 		m.merge("c:\\tmp\\1.txt", "c:\\tmp\\2.txt", "c:\\tmp\\dir\\2.txt"); 
+		System.out.println("Операция склеивания завершена."); 
 	}
 }
