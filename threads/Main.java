0e4cdc07647cd4bfa456c2dad85130f49e636bbc
montonaro <montonaro@gmail.com>
2014-04-01 13:33:11 +0300

    01.04.2014

--- threads/Main.java
+++ threads/Main.java
@@ -1,10 +1,22 @@
 package threads;
 
+
 public class Main {
 
 	public static void main(String[] args) {
 		 
-
+		MainThread main = new MainThread();
+		main.start();
+		
+		try {
+            		Thread.sleep(10000); 
+            		System.out.println("Конец!");
+        		main.interrupt(); 
+        	 catch (InterruptedException e) { 
+           		return; 
+        	 } 
+		
+		 
 	}
 
 }
