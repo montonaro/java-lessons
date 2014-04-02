61e2a2f8e1265eabef8463817df9b5b6f3a292f3
montonaro <montonaro@gmail.com>
2014-04-02 10:05:36 +0300

    02.04.2014

--- copyFile2/Main.java
+++ copyFile2/Main.java
@@ -1,15 +1,20 @@
-﻿package copyFile2;
+package copyFile2;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException, InterruptedException {
-
-
-		CopyFile2 c = new CopyFile2();
+	public static void main(String[] args) { 
 		
-		c.copyFile("c:\\tmp\\1.flv", "c:\\tmp\\dir\\1.flv");
+		CopyFile2 c = new CopyFile2(); 
+		try {
+			c.copyFile("c:\\tmp\\6279.mp4", "c:\\tmp\\dir\\6279.mp4");
+		} catch (FileNotFoundException e) {
+			System.out.println(e.getMessage());
+		} catch (IOException e) {
+			;
+		}
 		
 	}
 
