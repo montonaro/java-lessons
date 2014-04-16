b8d2cff108dfb9dbcb0cabecdd0b6377817b73a0
montonaro <montonaro@gmail.com>
2014-04-16 10:52:50 +0300

    Update Main.java

--- trans/Main.java
+++ trans/Main.java
@@ -1,6 +1,5 @@
 ﻿package translater;
-
-import java.util.Scanner;
+ 
 
 public class Main {
 	
@@ -10,34 +9,12 @@ public class Main {
 		Dictionary obj = new Dictionary();	 
 		
 		//obj.show(); // показать полностью что внутри
-		//obj.getNewWords();	
+		obj.getNewWords();	
 		System.out.println(" --------------------- ");  
-		//obj.show(); // показать полностью что внутри
+		//obj.show(); // показать полностью что внутри		
 		
-		
-		// для перевода
-		Scanner s 			= new Scanner(System.in);
-		String line 		= "";	   
-		
-		while(line == ""){
-			System.out.println("Введите строку для перевода.");  
-	    	line = s.nextLine().trim(); 
-	    	
-	    	if(line.length() > 0){    
-	    		 
-	    		String out = obj.toTranslate(line);	
-	    		System.out.println("Обработка завершена: " + line+" "+out);
-	    		
-	    	} else {
-	    		System.err.println("Ваша строка пуста! Попробуйте ввести еще раз."); 
-	    		line = "";
-	    	} 
-		}
-		 	 
-		if (s != null) {
-             s.close();
-        }  
-		
+		// для перевода 
+		obj.translate();  	
 		
 	}
 
